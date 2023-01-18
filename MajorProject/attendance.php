bb31d6de46456cf9368c26f367ccfc7da9a432b9
VISHESH SAXENA <visheshsaxena7777@gmail.com>
2023-01-18 13:53:23 +0530

    Update attendance.php

--- MajorProject/attendance.php
+++ MajorProject/attendance.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- SYTLE SHEET -->
-    <link rel="stylesheet" href="attendance.css">
+    <link rel="stylesheet" href="./css/attendance.css">
 
     <!-- CDN Library link -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
