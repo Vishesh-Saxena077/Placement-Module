62016da98a7c64fd4a9f234ae93cf94099eff3b5
VISHESH SAXENA <visheshsaxena7777@gmail.com>
2023-04-28 15:50:23 +0530

    resume
    resume link and its linking 4/28/2023

--- Major/student/create_resume_form.php
+++ Major/student/create_resume_form.php
@@ -1,36 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <title>Student | Acadmis | Create Resume Form</title>
+
     <!-- SYTLE SHEET LINK -->
     <link rel="stylesheet" href="./css/styleStudent.css">
-    <link rel="stylesheet" href="./css/ss.css">
+    <link rel="stylesheet" href="./css/create_resume.css">
 
     <!-- CDN Library link -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
     <!-- charts CDN -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
     <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
 
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+
+
     <!-- JQuery File for NavBar -->
-   <script src="./JS/JQFile.js"></script>
+    <script src="./JS/JQFile.js"></script>
 
     <!-- ICON LIBRARY -->
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
-
-    <!-- angular Js Library -->
-    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>
-
-    <title>Student | Dashboard</title>
 </head>
-
 <body onkeydown="if(!event.target.matches('input')&&!event.target.matches('textarea'))return!1" oncontextmenu="return!1" onselectstart="return!1" ondragstart="return!1">
 
-    <!-- Side Nav Bar -->
+ <!-- Side Nav Bar -->
     <nav class="sidebar close">
         <!-- NavBar Heading and Logo -->
         <header>
@@ -139,8 +138,8 @@
                         <div class="subdata item1" onclick="window.location.replace('sem_registration.php');">
                             Semester Registration
                         </div>
-                    
-                        <div class="subdata item1" onclick="window.location.replace('create_resume_form.php');">
+                        
+                        <div class="subdata item1" onclick="window.location.replace('https://www.overleaf.com/latex/templates/indian-institute-of-technology-bombay-resume/fgnpzhygqxrq.php');">
                             Create Your Resume
                         </div>
                     </div>
@@ -197,7 +196,6 @@
             </div>
         </div>
     </nav>
-
     <!-- Logout Confirmation Dialog Box -->
     <div class="overlay">
         <div class="dialogbase">
@@ -216,8 +214,7 @@
             </div>
         </div>
     </div>
-
-    <!-- Dashboard Content Box -->
+     <!-- Dashboard Content Box -->
     <section class="homePage">
         <div class="homePage-base">
 
@@ -225,15 +222,14 @@
             <div class="head">
                 <div class="head-1">
                     <div class="head-text">
-                        <div class="pannel-heading"> Dashboard </div>
+                        <div class="pannel-heading"> Create Resume </div>
                     </div>
                     <div class="Profile">
                         <div class="pro-1">
-                            <div class="pro-img" title="User Profile" onclick="window.location.replace('profile.php');">
-                                <img src="./images/male.jpg" alt="NA" height="100%" width="100%">
+                            <div class="pro-img" title="User Profile">
+                                <img src="../images/male.jpg" alt="NA" height="100%" width="100%">
                             </div>
                             <div class="pro-name">
-                                <i class="uil uil-setting" title="Settings"></i>
                                 <i class="uil uil-signout icon" title="Logout" id="logoutoption"></i>
                             </div>
                         </div>
@@ -244,104 +240,18 @@
             <!-- bread crums -->
             <div class="ss-head">
                 <div class="ss-inner-base">
-                    <h3> Dashboard </h3>
+                    <h3>Create Resume</h3>
                 </div>
             </div>
 
-            <!-- home Page Box-2 -->
-            <div class="box-2">
-                <div class="box-2_1">
-                    <div class="box-2_1_1">
-                        <div class="box-cont">
-                            <h1>Hi!
-                                <!-- user name -->
-                                <span>
-                                    Vishesh
-                                </span>
-                            </h1>
-                            <button title="button" name="learnmore" type="button" onclick="window.location.replce('training.php')">View More <i class="uil uil-angle-right"></i></button>
-                        </div>
-                    </div>
-                </div>
-            </div>
 
-            <div class="box-3">
-                <div class="box-3-innerbase">
-                    <div class="box-3-stats-base">
-                        <div class="box-3-content">
-                            <span>Semester</span>
-                            <span>7th</span>
-                        </div>
-                        <div class="box-3-avtar"><i class="uil uil-graduation-cap"></i></div>
+             <div class='main'> 
+                  <div class="s1_1" id="sec2">
+                    <div class="s1_2">
                     </div>
-                    <div class="box-3-stats-base">
-                        <div class="box-3-content">
-                            <span>Placement</span>
-                            <span>0</span>
-                        </div>
-                        <div class="box-3-avtar"><i class="uil uil-award-alt"></i></div>
-                    </div>
-                    <div class="box-3-stats-base">
-                        <div class="box-3-content">
-                            <span>Awards</span>
-                            <span>4</span>
-                        </div>
-                        <div class="box-3-avtar"><i class="uil uil-award"></i></div>
-                    </div>
-                    <div class="box-3-stats-base">
-                        <div class="box-3-content">
-                            <span>CGPA</span>
-                            <span>8.11</span>
-                        </div>
-                        <div class="box-3-avtar"><i class="uil uil-book-reader"></i></div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="v1_2">
-                <div class="v1_3">
-                    <div class="v1_4">
-                        <div class="v1_5">
-                            <div class="v2_6">
-                                <span class="v2_8">Notice</span>
-                            </div>
-                            <div class="display-notice">
-                                <div class="display-rows">
-                                    <h3 href="#">if any</h3>
-                                    <p href="#">Title</p>
-                                    <a href="#">if any</a>
-                                </div>
-                                <div class="display-rows">
-                                    <h3 href="#">if any</h3>
-                                    <p href="#">Title</p>
-                                    <a href="#">if any</a>
-                                </div>
-                                <div class="display-rows">
-                                    <h3 href="#">if any</h3>
-                                    <p href="#">Title</p>
-                                    <a href="#">if any</a>
-                                </div>
-                                <div class="display-rows">
-                                    <h3 href="#">if any</h3>
-                                    <p href="#">Title</p>
-                                    <a href="#">if any</a>
-                                </div>
-                            </div>
-
-                            <button type="button" title="more to know" onclick="window.location.replce('notice.php')">More...</button>
-
-                        </div>
-                    </div>
-                </div>
-            </div>
+                  </div>
+             </div>
         </div>
-    </section>
-
-    <!-- JavaScript file for Theme Changing -->
-
-    <script src="./JS/javascript.js"></script>
-
-
+</section>
 </body>
-
 </html>
