b4a95f52044a8a6f00e8fb18c96f294e65a74f87
VISHESH SAXENA <visheshsaxena7777@gmail.com>
2023-05-16 13:44:39 +0530

    time table added in display student and

---
+++ Major/admin/createtest.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="">
+        <!-- QUESTION 1 -->
+        <input type="text" name="q1" id="q1">
+        <input type="text" name="q1_o1" id="q1_o1">
+        <input type="text" name="q1_o2" id="q1_o2">
+        <input type="text" name="q1_o3" id="q1_o3">
+        <input type="text" name="q1_o4" id="q1_o4">
+
+        <!-- QUESTION 1 -->
+        <input type="text" name="q1" id="q1">
+        <input type="text" name="q1_o1" id="q1_o1">
+        <input type="text" name="q1_o2" id="q1_o2">
+        <input type="text" name="q1_o3" id="q1_o3">
+        <input type="text" name="q1_o4" id="q1_o4">
+
+        <!-- QUESTION 1 -->
+        <input type="text" name="q1" id="q1">
+        <input type="text" name="q1_o1" id="q1_o1">
+        <input type="text" name="q1_o2" id="q1_o2">
+        <input type="text" name="q1_o3" id="q1_o3">
+        <input type="text" name="q1_o4" id="q1_o4">
+        
+        <!-- QUESTION 1 -->
+        <input type="text" name="q1" id="q1">
+        <input type="text" name="q1_o1" id="q1_o1">
+        <input type="text" name="q1_o2" id="q1_o2">
+        <input type="text" name="q1_o3" id="q1_o3">
+        <input type="text" name="q1_o4" id="q1_o4">
+        
+        <!-- QUESTION 1 -->
+        <input type="text" name="q1" id="q1">
+        <input type="text" name="q1_o1" id="q1_o1">
+        <input type="text" name="q1_o2" id="q1_o2">
+        <input type="text" name="q1_o3" id="q1_o3">
+        <input type="text" name="q1_o4" id="q1_o4">
+        
+        <!-- QUESTION 1 -->
+        <input type="text" name="q1" id="q1">
+        <input type="text" name="q1_o1" id="q1_o1">
+        <input type="text" name="q1_o2" id="q1_o2">
+        <input type="text" name="q1_o3" id="q1_o3">
+        <input type="text" name="q1_o4" id="q1_o4">
+
+    </form>
+</body>
+</html>
---
+++ Major/student/timetable.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Timetable</title>
+    <style>
+      table, th, td {
+        border: 1px solid black;
+        border-collapse: collapse;
+      }
+      th, td {
+        padding: 8px;
+        text-align: center;
+      }
+      th {
+        background-color: #4CAF50;
+        color: white;
+      }
+    </style>
+  </head>
+  <body>
+    <center>
+    <table>
+	<tr>
+          <td colspan="6" style="color:white; background-color:black">Employee Information</td>
+      </tr>
+      <tr>
+        <th>Time</th>
+        <th>Monday</th>
+        <th>Tuesday</th>
+        <th>Wednesday</th>
+        <th>Thursday</th>
+        <th>Friday</th>
+      </tr>
+      <tr>
+        <th>8:00am - 9:00am</th>
+        <td>Mobile Computing</td>
+        <td>Disaster Management</td>
+        <td>Soft Computing</td>
+        <td>Mobile Applications Development</td>
+        <td>Agile Software Engineering</td>
+      </tr>
+      <tr>
+        <th>9:00am - 10:00am</th>
+        <td>Storage Networks</td>
+        <td>Unix Systems Programming</td>
+        <td>Mobile Computing</td>
+        <td>Soft Computing</td>
+        <td>library</td>
+      </tr>
+      <tr>
+        <th>10:00am - 11:00am</th>
+        <td>library</td>
+        <td>Storage Networks</td>
+        <td>Unix Systems Programming</td>
+        <td>Soft Computing</td>
+        <td>Physical Education</td>
+      </tr>
+	<tr>
+        <th>11:00am - 12:00am</th>
+        <td>library</td>
+        <td>Storage Networks</td>
+        <td>Unix Systems Programming</td>
+        <td>Soft Computing</td>
+        <td>Physical Education</td>
+      </tr>
+	<tr>
+        <th>12:00pm - 2:00pm</th>
+        <td>library</td>
+        <td>Storage Networks</td>
+        <td>Unix Systems Programming</td>
+        <td>Soft Computing</td>
+        <td>Physical Education</td>
+      </tr>
+	<tr>
+        <th>2:00pm - 3:00pm</th>
+        <td>library</td>
+        <td>Storage Networks</td>
+        <td>Unix Systems Programming</td>
+        <td>Soft Computing</td>
+        <td>Physical Education</td>
+      </tr>
+	<tr>
+        <th>3:00pm - 4:00pm</th>
+        <td>library</td>
+        <td>Storage Networks</td>
+        <td>Unix Systems Programming</td>
+        <td>Soft Computing</td>
+        <td>Physical Education</td>
+      </tr>
+	<tr>
+        <th>4:00pm - 5:00pm</th>
+        <td>library</td>
+        <td>Storage Networks</td>
+        <td>Unix Systems Programming</td>
+        <td>Soft Computing</td>
+        <td>Physical Education</td>
+      </tr>
+    </table>
+   </center>
+  </body>
+</html>
