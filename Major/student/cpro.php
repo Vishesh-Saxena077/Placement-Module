193f7285a605d2fb9af02e01b9a535c95e1739c1
VISHESH SAXENA <visheshsaxena7777@gmail.com>
2023-11-21 23:36:27 +0530

    c
    c

--- Major/student/cpro.php
+++ Major/student/cpro.php
@@ -8,7 +8,7 @@
 
     <!-- SYTLE SHEET LINK -->
     <link rel="stylesheet" href="./css/styleStudent.css">
-    <link rel="stylesheet" href="./css/training_Style.css">
+    <link rel="stylesheet" href="./css/cpro.css">
 
     <!-- CDN Library link -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
@@ -17,17 +17,20 @@
     <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
 
     <!-- JQuery File for NavBar -->
-   <script src="./JS/JQFile.js"></script>
+    <script src="./JS/JQFile.js"></script>
 
     <!-- ICON LIBRARY -->
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
 
-    <title>Student | training</title>
+    <!-- angular Js Library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>
+
+    <title>Student | C++ programming Courses</title>
 </head>
 
 <body onkeydown="if(!event.target.matches('input')&&!event.target.matches('textarea'))return!1" oncontextmenu="return!1" onselectstart="return!1" ondragstart="return!1">
 
-    <!-- Side Nav Bar -->
+ <!-- Side Nav Bar -->
     <nav class="sidebar close">
         <!-- NavBar Heading and Logo -->
         <header>
@@ -136,7 +139,7 @@
                         <div class="subdata item1" onclick="window.location.replace('sem_registration.php');">
                             Semester Registration
                         </div>
-                    
+                       
                         <div class="subdata item1" onclick="window.location.replace('create_resume_form.php');">
                             Create Your Resume
                         </div>
@@ -152,11 +155,8 @@
                         <div class="subdata item1" onclick="window.location.replace('companyApplied.php');">
                             Apply Company
                         </div>
-                        <div class="subdata item1" onclick="window.location.replace('traning.php');">
+                        <div class="subdata item1" onclick="window.location.replace('training.php');">
                             Training
-                        </div>
-                        <div class="subdata item1" onclick="window.location.replace('test_login.php');">
-                            Take Test
                         </div>
                     </div>
                 </div>
@@ -217,7 +217,7 @@
         </div>
     </div>
 
-    <!-- Dashboard Content Box -->
+  <!-- Dashboard Content Box -->
     <section class="homePage">
         <div class="homePage-base">
 
@@ -225,12 +225,12 @@
             <div class="head">
                 <div class="head-1">
                     <div class="head-text">
-                        <div class="pannel-heading"> Dashboard </div>
+                        <div class="pannel-heading"> C++ Programming</div>
                     </div>
                     <div class="Profile">
                         <div class="pro-1">
-                            <div class="pro-img" title="User Profile" onclick="window.location.replace('profile.php');">
-                                <img src="./images/male.jpg" alt="NA" height="100%" width="100%">
+                            <div class="pro-img" title="User Profile">
+                                <img src="../images/male.jpg" alt="NA" height="100%" width="100%">
                             </div>
                             <div class="pro-name">
                                 <i class="uil uil-setting" title="Settings"></i>
@@ -244,65 +244,63 @@
             <!-- bread crums -->
             <div class="ss-head">
                 <div class="ss-inner-base">
-                    <h3> Dashboard </h3>
+                    <h3>C++ Programing</h3>
                 </div>
             </div>
 
-            
 
-            <div class="box-3">
-                <div class="box-3-innerbase">
-                    
-                    <div class="box-3-stats-base">
-                        <div class="box-3-content">
-                            <span>C++</span>
-                            <span></span>
-                        </div>
-                        <div class="box-3-avtar">
-                            <button onclick="window.location.replace('cplus.php');">Start Now</button>
-                        </div>
-                    </div>
-
-                    <div class="box-3-stats-base">
-                        <div class="box-3-content">
-                            <span>Python</span>
-                            <span></span>
-                        </div>
-                        <div class="box-3-avtar">
-                            <button onclick="window.location.replace('python.php');">Start Now</button>
-                        </div>
-                    </div>
-                    
-                    <div class="box-3-stats-base">
-                        <div class="box-3-content">
-                            <span>JAVA</span>
-                            <span></span>
-                        </div>
-                        <div class="box-3-avtar">
-                            <button onclick="window.location.replace('java.php');">Start Now</button>
-                        </div>
-                    </div>
-                    
-                    <div class="box-3-stats-base">
-                        <div class="box-3-content">
-                            <span>C Programming</span>
-                            <span></span>
-                        </div>
-                        <div class="box-3-avtar">
-                            <button onclick="window.location.replace('cpro.php');">Start Now</button>
-                        </div>
-                    </div>
-                    
+             <div class='main'> 
+                <div class="C_content">
+                    <p><b>C++</b> is a middle-level programming language developed by Bjarne Stroustrup starting in 1979 at Bell Labs. <b>C++</b> runs on a variety of platforms, such as Windows, Mac OS, and the various versions of UNIX. This <b>C++</b> tutorial adopts a simple and practical approach to describe the concepts of <b>C++</b> for beginners to advanded software engineers.</p><br><br>
+                    <h2>Why to Learn C++</h2><br>
+                    <p><b>C++</b> is a MUST for students and working professionals to become a great Software Engineer. I will list down some of the key advantages of learning C++:</p>
+                    <ul class="list">
+                        <li><p>C++ is very close to hardware, so you get a chance to work at a low level which gives you lot of control in terms of memory management, better performance and finally a robust software development.</p></li>
+                        <li><p><b>C++ programming</b> gives you a clear understanding about Object Oriented Programming. You will understand low level implementation of polymorphism when you will implement virtual tables and virtual table pointers, or dynamic type identification.</p></li>
+                        <li><p>C++ is one of the every green programming languages and loved by  millions of software developers. If you are a great C++ programmer then you will never sit without work and more importantly you will get highly paid for your work.</p></li>
+                        <li><p>C++ is the most widely used programming languages in application and system programming. So you can choose your area of interest of software development.</p></li>
+                        <li><p>C++ really teaches you the difference between compiler, linker and loader, different data types, storage classes, variable types their scopes etc.</p></li>
+                    </ul>
+                    <p>There are 1000s of good reasons to learn C++ Programming. But one thing for sure, to learn any programming language, not only C++, you just need to code, and code and finally code until you become expert.</p><br><br>
+                    <h2>Hello World using C++</h2>
+                    <p>Just to give you a little excitement about <b>C++ programming</b>, I'm going to give you a small conventional C++ Hello World program, You can try it using Demo link</p>
+                    <a href="https://www.educative.io/blog/top-cpp-coding-interview-questions"> learn more</a>
+                    <br><br>
+                    <h2>link for practise question</h2>
+                    <ol>
+                        <li><a href="https://practice.geeksforgeeks.org/problems/subarray-with-given-sum/0">Subarray with given sum</a></li>
+                        <li><a href="https://practice.geeksforgeeks.org/problems/count-the-triplets/0" rel="noopener" target="_blank">Count the triplets</a></li>
+                        <li><a href="https://practice.geeksforgeeks.org/problems/kadanes-algorithm/0">Kadane’s Algorithm</a></li>
+                        <li><a href="https://practice.geeksforgeeks.org/problems/missing-number-in-array/0">Missing number in array</a></li>
+                        <li><a href="https://practice.geeksforgeeks.org/problems/merge-two-sorted-arrays/0/" rel="noopener" target="_blank">Merge two sorted arrays</a></li>
+                        <li><a href="https://practice.geeksforgeeks.org/problems/-rearrange-array-alternately/0/" rel="noopener" target="_blank">Rearrange array alternatively</a></li>
+                        <li><a href="https://practice.geeksforgeeks.org/problems/number-of-pairs/0/" rel="noopener" target="_blank">Number of pairs</a></li>
+                        <li><a href="https://practice.geeksforgeeks.org/problems/inversion-of-array/0/" rel="noopener" target="_blank">Inversion of Array</a></li>
+                        <li><a href="https://practice.geeksforgeeks.org/problems/sort-an-array-of-0s-1s-and-2s/0">Sort an array of 0s, 1s and 2s</a></li>
+                        <li><a href="https://practice.geeksforgeeks.org/problems/equilibrium-point/0">Equilibrium point</a></li>
+                        <li><a href="https://practice.geeksforgeeks.org/problems/leaders-in-an-array/0">Leaders in an array</a></li>
+                        <li><a href="https://practice.geeksforgeeks.org/problems/minimum-platforms/0">Minimum Platforms</a></li>
+                        <li><a href="https://practice.geeksforgeeks.org/problems/reverse-array-in-groups/0">Reverse array in groups</a></li>
+                        <li><a href="https://practice.geeksforgeeks.org/problems/kth-smallest-element/0">K’th smallest element</a></li>
+                        <li><a href="https://practice.geeksforgeeks.org/problems/trapping-rain-water/0">Trapping Rain Water</a></li>
+                        <li><a href="https://practice.geeksforgeeks.org/problems/pythagorean-triplet/0">Pythagorean Triplet</a></li>
+                        <li><a href="https://practice.geeksforgeeks.org/problems/chocolate-distribution-problem/0">Chocolate Distribution Problem</a></li>
+                        <li><a href="https://practice.geeksforgeeks.org/problems/stock-buy-and-sell/0">Stock buy and sell</a></li>
+                        <li><a href="https://practice.geeksforgeeks.org/problems/unsorted-array/0">Element with left side smaller and right side greater</a></li>
+                        <li><a href="https://practice.geeksforgeeks.org/problems/convert-array-into-zig-zag-fashion/0">Convert array into Zig-Zag fashion</a></li>
+                        <li><a href="https://practice.geeksforgeeks.org/problems/last-index-of-1/0">Last Index of 1</a></li>
+                        <li><a href="https://practice.geeksforgeeks.org/problems/spirally-traversing-a-matrix/0">Spirally traversing a matrix</a></li>
+                        <li><a href="https://practice.geeksforgeeks.org/problems/largest-number-formed-from-an-array/0">Largest Number formed from an Array</a></li>
+                    </ol>
                 </div>
-            </div>
+             </div>
 
-            
-        </div>
     </section>
 
     <!-- JavaScript file for Theme Changing -->
 
     <script src="./JS/javascript.js"></script>
+
 
 
 </body>
--- Major/student/java.php
+++ Major/student/java.php
@@ -8,7 +8,7 @@
 
     <!-- SYTLE SHEET LINK -->
     <link rel="stylesheet" href="./css/styleStudent.css">
-    <link rel="stylesheet" href="./css/training_Style.css">
+    <link rel="stylesheet" href="./css/cpr.css">
 
     <!-- CDN Library link -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
@@ -17,17 +17,20 @@
     <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
 
     <!-- JQuery File for NavBar -->
-   <script src="./JS/JQFile.js"></script>
+    <script src="./JS/JQFile.js"></script>
 
     <!-- ICON LIBRARY -->
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
 
-    <title>Student | training</title>
+    <!-- angular Js Library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>
+
+    <title>Student | C++ programming Courses</title>
 </head>
 
 <body onkeydown="if(!event.target.matches('input')&&!event.target.matches('textarea'))return!1" oncontextmenu="return!1" onselectstart="return!1" ondragstart="return!1">
 
-    <!-- Side Nav Bar -->
+ <!-- Side Nav Bar -->
     <nav class="sidebar close">
         <!-- NavBar Heading and Logo -->
         <header>
@@ -136,7 +139,7 @@
                         <div class="subdata item1" onclick="window.location.replace('sem_registration.php');">
                             Semester Registration
                         </div>
-                    
+                       
                         <div class="subdata item1" onclick="window.location.replace('create_resume_form.php');">
                             Create Your Resume
                         </div>
@@ -152,11 +155,8 @@
                         <div class="subdata item1" onclick="window.location.replace('companyApplied.php');">
                             Apply Company
                         </div>
-                        <div class="subdata item1" onclick="window.location.replace('traning.php');">
+                        <div class="subdata item1" onclick="window.location.replace('training.php');">
                             Training
-                        </div>
-                        <div class="subdata item1" onclick="window.location.replace('test_login.php');">
-                            Take Test
                         </div>
                     </div>
                 </div>
@@ -217,7 +217,7 @@
         </div>
     </div>
 
-    <!-- Dashboard Content Box -->
+  <!-- Dashboard Content Box -->
     <section class="homePage">
         <div class="homePage-base">
 
@@ -225,12 +225,12 @@
             <div class="head">
                 <div class="head-1">
                     <div class="head-text">
-                        <div class="pannel-heading"> Dashboard </div>
+                        <div class="pannel-heading"> C++ Programming</div>
                     </div>
                     <div class="Profile">
                         <div class="pro-1">
-                            <div class="pro-img" title="User Profile" onclick="window.location.replace('profile.php');">
-                                <img src="./images/male.jpg" alt="NA" height="100%" width="100%">
+                            <div class="pro-img" title="User Profile">
+                                <img src="../images/male.jpg" alt="NA" height="100%" width="100%">
                             </div>
                             <div class="pro-name">
                                 <i class="uil uil-setting" title="Settings"></i>
@@ -244,65 +244,97 @@
             <!-- bread crums -->
             <div class="ss-head">
                 <div class="ss-inner-base">
-                    <h3> Dashboard </h3>
+                    <h3>C++ Programing</h3>
                 </div>
             </div>
 
-            
 
-            <div class="box-3">
-                <div class="box-3-innerbase">
-                    
-                    <div class="box-3-stats-base">
-                        <div class="box-3-content">
-                            <span>C++</span>
-                            <span></span>
-                        </div>
-                        <div class="box-3-avtar">
-                            <button onclick="window.location.replace('cplus.php');">Start Now</button>
-                        </div>
+             <div class='main'> 
+                <div class="C_content">
+                    <h1 class="h1">Java Tutorial</h1>
+                    <p>Our core Java programming tutorial is designed for students and working professionals. Java is an <a href="java-oops-concepts">object-oriented</a>, class-based, concurrent, secured and general-purpose computer-programming language. It is a widely used robust technology. </p><br><br>
+                    <h2 id="what-is-java" class="h2">What is Java?</h2>
+                    <p>Java is a <strong>programming language</strong> and a <strong>platform</strong>. Java is a high level, robust, object-oriented and secure programming language.</p>
+                    <p>Java was developed by <em>Sun Microsystems</em> (which is now the subsidiary of Oracle) in the year 1995. <em>James Gosling</em> is known as the father of Java. Before Java, its name was <em>Oak</em>. Since Oak was already a registered company, so James Gosling and his team changed the name from Oak to Java.</p>
+                    <p><strong>Platform</strong>: Any hardware or software environment in which a program runs, is known as a platform. Since Java has a runtime environment (JRE) and API, it is called a platform.</p>
+                    <br><br>
+                    <h2 id="java-example" class="h2">Java Example</h2>
+                    <p>Let's have a quick look at Java programming example. A detailed description of Hello Java example is available in next page.</p>
+                    <p><strong>Simple.java</strong></p>
+                    <div class="codeblock">
+                        <textarea name="code" class="java" rows="5" cols="45">
+                            class Simple{
+                                public static void main(String args[]){
+                                System.out.println("Hello Java");
+                                }
+                            }
+                        </textarea>
                     </div>
+                    <span class="testit"><a href="https://compiler.javatpoint.com/opr/test.jsp?filename=Simple" target="_blank">Test it Now</a></span>
+                    <br><br>
+                    <h2 id="wjiu" class="h2">Application</h2>
+                    <p>According to Sun, 3 billion devices run Java. There are many devices where Java is currently used. Some of them are as follows:</p>
+                    <ol class="points">
+                        <li>Desktop Applications such as acrobat reader, media player, antivirus, etc.</li>
+                        <li>Web Applications such as irctc.co.in, javatpoint.com, etc.</li>
+                        <li>Enterprise Applications such as banking applications.</li>
+                        <li>Mobile</li>
+                        <li>Embedded System</li>
+                        <li>Smart Card</li>
+                        <li>Robotics</li>
+                        <li>Games, etc.</li>
+                    </ol><br>   
+                    <h2 id="java-applications" class="h2">Types of Java Applications</h2>
+                    <p>There are mainly 4 types of applications that can be created using Java programming:</p><br>
+                    <h4 class="h4">Standalone Application</h4>
+                    <p>Standalone applications are also known as desktop applications or window-based applications. These are traditional software that we need to install on every machine. Examples of standalone application are Media player, antivirus, etc. AWT and Swing are used in Java for creating standalone applications.</p>
+                    <h4 class="h4">Web Application</h4>
+                    <p>An application that runs on the server side and creates a dynamic page is called a web application. Currently, <a href="servlet-tutorial">Servlet</a>, <a href="jsp-tutorial">JSP</a>, <a href="struts-2-tutorial">Struts</a>, <a href="spring-tutorial">Spring</a>, <a href="hibernate-tutorial">Hibernate</a>, <a href="jsf-tutorial">JSF</a>, etc. technologies are used for creating web applications in Java.</p>
+                    <h4 class="h4">Enterprise Application</h4>
+                    <p>An application that is distributed in nature, such as banking applications, etc. is called an enterprise application. It has advantages like high-level security, load balancing, and clustering. In Java, <a href="https://www.javatpoint.com/ejb-tutorial">EJB</a> is used for creating enterprise applications.</p>
+                    <h4 class="h4">Mobile Application</h4>
+                    <p>An application which is created for mobile devices is called a mobile application. Currently, Android and Java ME are used for creating mobile applications.</p><br>
+                    <h2 id="java-platforms" class="h2">Java Platforms / Editions</h2>
+                    <p>There are 4 platforms or editions of Java:</p>
+                    <h4 class="h4">Java SE (Java Standard Edition)</h4>
+                    <p>It is a Java programming platform. It includes Java programming APIs such as java.lang, java.io, java.net, java.util, java.sql, java.math etc. It includes core topics like OOPs, <a href="java-string">String</a>, Regex, Exception, Inner classes, Multithreading, I/O Stream, Networking, AWT, Swing, Reflection, Collection, etc.</p><br>
+                    <h4 class="h4">Java EE (Java Enterprise Edition)</h4>
+                    <p>It is an enterprise platform that is mainly used to develop web and enterprise applications. It is built on top of the Java SE platform. It includes topics like Servlet, JSP, Web Services, EJB, <a href="https://www.javatpoint.com/jpa-tutorial">JPA</a>, etc.</p><br>
+                    <h4 class="h4">Java ME (Java Micro Edition)</h4>
+                    <p>It is a micro platform that is dedicated to mobile applications.</p><br>
+                    <h4 class="h4">JavaFX</h4>
+                    <p>It is used to develop rich internet applications. It uses a lightweight user interface API.</p><br>
+                    <h2 class="h2">Prerequisite</h2>
+                    <p>To learn Java, you must have the basic knowledge of C/C++ programming language.</p><br>
+                    <h2 class="h2">Audience</h2>
+                    <p>Our Java programming tutorial is designed to help beginners and professionals.</p><br>
+                    <h2 class="h2">Problem</h2>
+                    <p>We assure that you will not find any problem in this Java tutorial. However, if there is any mistake, please post the problem in the contact form.</p>
+                    <br /><br />
 
-                    <div class="box-3-stats-base">
-                        <div class="box-3-content">
-                            <span>Python</span>
-                            <span></span>
-                        </div>
-                        <div class="box-3-avtar">
-                            <button onclick="window.location.replace('python.php');">Start Now</button>
-                        </div>
-                    </div>
-                    
-                    <div class="box-3-stats-base">
-                        <div class="box-3-content">
-                            <span>JAVA</span>
-                            <span></span>
-                        </div>
-                        <div class="box-3-avtar">
-                            <button onclick="window.location.replace('java.php');">Start Now</button>
-                        </div>
-                    </div>
-                    
-                    <div class="box-3-stats-base">
-                        <div class="box-3-content">
-                            <span>C Programming</span>
-                            <span></span>
-                        </div>
-                        <div class="box-3-avtar">
-                            <button onclick="window.location.replace('cpro.php');">Start Now</button>
-                        </div>
-                    </div>
-                    
+
+                    <h2> java practise question link</h2>
+                    <ol>
+
+                        <li class="h4"><a href="https://www.javatpoint.com/fibonacci-series-in-java">Fibonacci Series in Java</a></li>
+                        <li class="h4"><a href="https://www.javatpoint.com/prime-number-program-in-java">Prime Number Program in Java</a></li>
+                        <li class="h4"><a href="https://www.javatpoint.com/palindrome-program-in-java">Palindrome Program in Java</a></li>
+                        <li class="h4"><a href="https://www.javatpoint.com/factorial-program-in-java">Factorial Program in Java</a></li>
+                        <li class="h4"><a href="https://www.javatpoint.com/armstrong-number-in-java">Armstrong Number in Java</a></li>
+                        <li class="h4"><a href="https://www.javatpoint.com/how-to-generate-random-number-in-java">How to Generate Random Number in Java</a></li>
+                        <li class="h4"><a href="https://www.javatpoint.com/how-to-print-pattern-in-java">How to Print Pattern in Java</a></li>
+                        <li class="h4"><a href="https://www.javatpoint.com/how-to-compare-two-objects-in-java">How to Compare Two Objects in Java</a></li>
+                        <li class="h4"><a href="https://www.javatpoint.com/how-to-create-object-in-java">How to Create Object in Java</a></li>
+                        <li class="h4"><a href="https://www.javatpoint.com/how-to-print-ascii-value-in-java">How to Print ASCII Value in Java</a></li>
+                    </ol>
                 </div>
-            </div>
-
-            
-        </div>
+             </div>
     </section>
 
     <!-- JavaScript file for Theme Changing -->
 
     <script src="./JS/javascript.js"></script>
+
 
 
 </body>
